9f87d66d096edb1cc076b1b1b736f9c5565b99cb
Luiz Lopes <luizlopes12@outlook.com>
2021-12-11 11:51:59 -0300

    deixando mais bunitin

--- click-count/src/components/count.js
+++ click-count/src/components/count.js
@@ -1,14 +1,21 @@
-import React, {useState, useEffect, Fragment} from 'react'
+import React, {useState, useEffect, Fragment} from 'react';
+import './style.css';
 function Count(){
     const [contador, setContador] = useState(0)
     let add = () => setContador(contador + 1)
+    const getRandomColor = ()=>{
+        return "#" + Math.random().toString(16).slice(2,8);
+     }
     useEffect(() =>{
         document.title = `Você clicou ${contador} vezes`
+        document.body.style.backgroundColor = getRandomColor();
     })
     return(
         <Fragment>
+            <div className="cont">
             <p>Você clicou {contador} vezes</p>
             <button onClick={add}>Click</button>
+            </div>
         </Fragment>
     )
 }
