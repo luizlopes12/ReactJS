90bb35f887887205572508f4f258b575f5292d8b
Luiz Lopes <luizlopes12@outlook.com>
2021-12-11 11:36:28 -0300

    react app

---
+++ click-count/src/App.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+import {Count} from 'components/count'
+function App() {
+  return (
+    <Count/>
+  );
+}
+
+export default App;
---
+++ click-count/src/components/count.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect, Fragment} from 'react'
+function Count(){
+    const [contador, setContador] = useState(0)
+    
+
+
+    
+}
+
+
+
+
+export {Count}
