2911f84d683378afce5083e2a0c5c547647bb213
Luiz Lopes <luizlopes12@outlook.com>
2021-12-11 11:43:48 -0300

    foi

--- click-count/src/App.js
+++ click-count/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Count} from 'components/count'
+import {Count} from './components/count'
 function App() {
   return (
     <Count/>
--- click-count/src/components/count.js
+++ click-count/src/components/count.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect, Fragment} from 'react'
 function Count(){
     const [contador, setContador] = useState(0)
-    
-
-
-    
+    let add = () => setContador(contador + 1)
+    useEffect(() =>{
+        document.title = `Você clicou ${contador} vezes`
+    })
+    return(
+        <Fragment>
+            <p>Você clicou {contador} vezes</p>
+            <button onClick={add}>Click</button>
+        </Fragment>
+    )
 }
-
-
-
-
 export {Count}
