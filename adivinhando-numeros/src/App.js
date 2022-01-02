5ac81680ae7564e8ce009fe8e5ec7703e45269e8
Luiz Lopes <luizlopes12@outlook.com>
2022-01-02 16:11:11 -0300

    montando o projeto

--- adivinhando-numeros/src/App.js
+++ adivinhando-numeros/src/App.js
@@ -2,14 +2,18 @@ import React, {useState} from 'react'
 function App() {
   //Estados: entrada, rodando, fim
   const [estado, setEstado] = useState('Entrada')
+  //palpite
+  const [palpite, setPalpite] = useState(150)
+
   const iniciarJogo = () =>{
     setEstado('Rodando')
   }
   if(estado == 'Entrada'){
-    return <button>Começar a jogar</button>
+    return <button onClick={iniciarJogo}>Começar a jogar</button>
   }
   return (
     <div className="App">
+    <p>O seu número é {palpite}?</p>
     <button>Menor</button>
     <button>Acertou</button>
     <button>Maior</button>
