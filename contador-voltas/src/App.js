78ea4a565a548f54294366c0fa774cae43c3ffd6
Luiz Lopes <luizlopes12@outlook.com>
2022-01-03 08:17:34 -0300

    components

--- contador-voltas/src/App.js
+++ contador-voltas/src/App.js
@@ -1,9 +1,30 @@
-
-
+import React from 'react'
 function App() {
+  const MostraVoltas = (props) => {
+    return(
+      <p>
+      {props.voltas}
+      <br/>
+      Voltas
+      </p>
+    )
+  }
+  const Tempo = (props) =>{
+    <p>
+    {props.tempo}<br/>
+    Tempo médio por volta
+    </p>
+  }
   return (
-    
-  );
+    <div className='App'>
+    <MostraVoltas voltas='12'/>
+    <button>+</button>
+    <button>-</button>
+    <Tempo tempo='12:25'/>
+    <button>Iniciar</button>
+    <button>Reiniciar</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
