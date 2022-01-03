e433b11ea90ddbde80350ed99311b76531b68e19
Luiz Lopes <luizlopes12@outlook.com>
2022-01-03 08:42:40 -0300

    Update App.js

--- contador-voltas/src/App.js
+++ contador-voltas/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react'
-function App() {
+import React, {useState, useEffect} from 'react'
   const MostraVoltas = (props) => {
     return(
       <p>
@@ -10,19 +9,36 @@ function App() {
     )
   }
   const Tempo = (props) =>{
+    return(
     <p>
     {props.tempo}<br/>
     Tempo médio por volta
     </p>
+    )
+  }
+  const Button = (props)=> <button onClick={props.action}>{props.text}</button>
+  function App() {
+  const [numVoltas, setVoltas] = useState(0)
+  const [tempo, setTempo] = useState(0)
+  useEffect(()=>{
+    setInterval(()=>{
+      console.log('opa')
+    },1000)
+  },[])
+  const increment = () =>{
+    setVoltas(numVoltas+1)
+  }
+  const decrement = () =>{
+    setVoltas(numVoltas-1)
   }
   return (
     <div className='App'>
-    <MostraVoltas voltas='12'/>
-    <button>+</button>
-    <button>-</button>
-    <Tempo tempo='12:25'/>
-    <button>Iniciar</button>
-    <button>Reiniciar</button>
+    <MostraVoltas voltas={numVoltas}/>
+    <Button text='+' action={increment}/>
+    <Button text='-' action={decrement}/>
+    <Tempo tempo={tempo}/>
+    <Button text='Iniciar'/>
+    <Button text='Reiniciar'/>
     </div>
   )
 }
