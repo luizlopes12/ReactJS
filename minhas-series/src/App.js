027b7b2b2a7299cbf03b6ab15f68b3983441a094
Luiz Lopes <luizlopes12@outlook.com>
2022-01-04 08:55:20 -0300

    install react router and navbar

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,8 +1,40 @@
+import React, {useState} from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap'
+
+const Header = () =>{
+  const [open, setOpen] = useState(false)
+  const toggle = () =>{
+    setOpen(!open)
+  }
+  
+  return (
+    <div>
+      <Navbar color='light' light expand='md'>
+        <NavbarBrand>Minhas Séries</NavbarBrand>
+        <NavbarToggler onClick={toggle}/>
+          <Collapse isOpen={open} navbar>
+            <Nav className='ml-auto' navbar>
+            <NavItem>
+              <NavLink href='/'>Gêneros</NavLink>            
+            </NavItem>
+            </Nav>
+          </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>Minhas séries</h1>
-    </div>
+    <Header/>
   );
 }
 
